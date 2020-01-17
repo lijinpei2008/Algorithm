2b3a35ef7c7b2a61a9b8c872b65e9624b19f4742
lijinpei2008 <jinpeili@.github.com>
2020-01-17 17:40:37 +0800

    Recursion:2.3

--- Easy/Recursion 递归/Pascal'sTriangle.cs
+++ Easy/Recursion 递归/Pascal'sTriangle.cs
@@ -325,3 +325,76 @@ namespace PascalsTriangle {
         }
     }
 }
+
+// On the first row, we write a 0. Now in every subsequent row.
+// we look at the previous row and replace each occurrence of 0 with 01, and each occurrence of 1 with 10.
+// Given row N and index K, return the K-th indexed symbol in row N. (The values of K are 1-indexed.) (1 indexed).
+
+// Examples:
+// Input: N = 1, K = 1
+// Output: 0
+
+// Input: N = 2, K = 1
+// Output: 0
+
+// Input: N = 2, K = 2
+// Output: 1
+
+// Input: N = 4, K = 5
+// Output: 1
+
+// Explanation:
+// row 1: 0
+// row 2: 01        row 1 : 0 => 01
+// row 3: 0110      row 2 : 0 => 01, 1 => 10
+// row 4: 01101001  row 3 : 0 => 01, 1 => 10, 1 => 10, 0 => 01
+using System;
+
+namespace PascalsTriangle {
+    public class Program {
+        static void Main(string[] args) {
+            Solution solution = new Solution();
+            int num = solution.KthGrammar(3, 1);
+            Console.WriteLine(num);
+        }
+    }
+
+    public class Solution {
+        public int KthGrammar(int N, int K) {
+            if (N == 1) {
+                return 0;
+            }
+            if (K % 2 == 0) {
+                return (KthGrammar(N - 1, K / 2) == 0 ? 1 : 0);
+            }
+            return (KthGrammar(N - 1, (K + 1) / 2) == 0 ? 0 : 1);
+        }
+    }
+}
+
+using System;
+
+namespace PascalsTriangle {
+    public class Program {
+        static void Main(string[] args) {
+            Solution solution = new Solution();
+            int num = solution.KthGrammar(3, 1);
+            Console.WriteLine(num);
+        }
+    }
+
+    public class Solution {
+        public int KthGrammar(int N, int K) {
+            if (N == 1) {
+                return 0;
+            }
+            int parentGrammar = KthGrammar(N - 1, (K + 1) / 2);
+            if (parentGrammar == 0) {
+                return K % 2 == 1 ? 0 : 1;
+            }
+            else {
+                return K % 2 == 1 ? 1 : 0;
+            }
+        }
+    }
+}
