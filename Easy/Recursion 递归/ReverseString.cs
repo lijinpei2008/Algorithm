ec9022a13849addcb814e64af06fc213d99b2234
lijinpei2008 <jinpeili@.github.com>
2020-01-17 10:27:07 +0800

    Recursion:v1.2

--- Easy/Recursion 递归/ReverseString.cs
+++ Easy/Recursion 递归/ReverseString.cs
@@ -97,7 +97,6 @@ class Solution :
             if left<right:
                 s[left], s[right] = s[right], s[left]
                 helper(left + 1, right - 1)
-
         helper(0, len(s) - 1)
 
 // Java
