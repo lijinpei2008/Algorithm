1c28f3d3457f2f093c6bf24256dc806d8b3b968b
lijinpei2008 <31384087+lijinpei2008@users.noreply.github.com>
2020-07-29 17:24:48 +0800

    Arithmetic:v2.2

--- LeetCodingChallenge/May/Week3.cs
+++ LeetCodingChallenge/May/Week3.cs
@@ -144,27 +144,22 @@ namespace MayThird {
 }
 
 //Given a string s and a non-empty string p, find all the start indices of p's anagrams in s.
-
 //Strings consists of lowercase English letters only and the length of both strings s and p will not be larger than 20,100.
-
 //The order of output does not matter.
 
 //Example 1:
-
 //Input:
 //s: "cbaebabacd" p: "abc"
-
 //Output:
 //[0, 6]
 
 //Explanation:
 //The substring with start index = 0 is "cba", which is an anagram of "abc".
 //The substring with start index = 6 is "bac", which is an anagram of "abc".
-//Example 2:
 
+//Example 2:
 //Input:
 //s: "abab" p: "ab"
-
 //Output:
 //[0, 1, 2]
 
